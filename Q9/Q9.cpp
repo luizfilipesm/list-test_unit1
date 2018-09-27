bff800cdba31b36f1aff408aef2225a9edc8caac
Luiz Filipe Mariz <lfsmariz@gmail.com>
2018-09-27 18:41:07 -0300

    Q9 Completa

--- Q9/Q9.cpp
+++ Q9/Q9.cpp
@@ -4,23 +4,38 @@ typedef bool (*Equal)( const void *, const void *);
 typedef unsigned char byte;
 
 //Função Find : (Início do Array, Final do Array, Tamanho dos elementos a serem testados, função predicado a ser verificada)
-const void *find( const void* first, const void* last, size_t size, const void* value, Equal eq )
+const void *Unique( const void* first, const void* last, size_t size, Equal eq )
 {
     //conversão para recebimento do primeiro elemento
     const byte *it = static_cast< const byte *>( first );//ponteiro para primeiro elemento apontando para início do array
-    const byte *valu = static_cast< const byte *>( value );//ponteiro para elemento base de busca
-    const byte *find_o = static_cast< const byte *>(last);  //ponteiro em last para caso não satisfaça o predicado 
+    const byte *last = static_cast< const byte *>(last);  //ponteiro em last para caso não satisfaça o predicado 
+    const byte *slow = it;
+    const byte *init = it;
+    const byte *verify = it;
     
-
     while( it != last ) //comparação posição do iterador em relação ao fim do array
     {
+        bool verification = true;
         
-        if ( eq( it , valu ) )//verificação com função predicado
+        while (verify != slow)
+        {
+            if (eq( verify , it ) )//verificação com função predicado
             {
-                find_o = it; //atualização do primeiro elemento que satisfaz a função
+                verification = false;
             } 
+        
+            verify += size;
+        }
+
+        if( verification)
+        {
+            memcpy( slow , it, size);
+            slow += size; 
+        }
+
+        verify = init;
+
         it += size; //progressão do iterador
     }
 
-    return find_o; //Retorna o endereço que satisfaz o predicado
 }
